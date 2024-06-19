29453c131cda6d014855e6c6a17c78a4fcd14bb7
morteza <m@yahooc.com>
2024-06-19 09:01:34 +0330

    refactor: toast composable

--- src/composables/toast/index.js
+++ src/composables/toast/index.js
@@ -2,8 +2,8 @@ import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
 
 export function useToast() {
-  const createToast = (toastTitle, toastOption) => {
-    toast(toastTitle, toastOption);
+  const createToast = (message, options) => {
+    toast(message, options);
   };
 
   return { createToast };
